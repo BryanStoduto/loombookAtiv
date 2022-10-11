1f10b6997673ab5f3071a78774f8f26b823afcbd
Bryan <67024498+BryanStoduto@users.noreply.github.com>
2022-10-11 14:20:42 -0300

    Add files via upload

---
+++ Atividades-master/lombook-conversor/src/main/java/com/ConversaoLombook/lombook/conversor/LombookConversorApplication.java
@@ -0,0 +1,13 @@
+package com.ConversaoLombook.lombook.conversor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombookConversorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LombookConversorApplication.class, args);
+	}
+
+}
---
+++ Atividades-master/lombook-conversor/src/test/java/com/ConversaoLombook/lombook/conversor/LombookConversorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ConversaoLombook.lombook.conversor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LombookConversorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
